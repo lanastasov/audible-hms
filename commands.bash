983fd3d7574b6768e2fb3190144cec975770fe96
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2023-01-21 21:09:15 +0200

    library export spteps

---
+++ commands.bash
@@ -0,0 +1,6 @@
+audible library list
+  B00AUVOMFS: Napoleon Hill: The Master Key to Riches
+audible download --asin B00AUVOMFS --aaxc --cover --cover-size 1215 --chapter
+audible download --asin B00AUVOMFS --aaxc --cover --cover-size 1215 
+./AAXtoMP3/AAXtoMP3 ./The_Master_Key_to_Riches.aaxc
+
